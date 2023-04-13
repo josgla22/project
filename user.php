a213bfcbc504b6bc72a1e6d7a83a8b3e717f5749
Josefine Glad <josefineglad@EngstroGladsMBP.lan>
2023-04-13 12:05:07 +0200

    type your commit message here

---
+++ user.php
@@ -0,0 +1,12 @@
+<?php
+include('template.php');
+$content = <<<END
+<h1>Welcome to this website</h1>
+<p>
+This is gonna be a webshop
+</p>
+END;
+echo $navigation;
+echo $content;
+include('footer.php');
+?>
